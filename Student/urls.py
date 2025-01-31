0145d003295034676e5cc0a628229b1b6e9ddfc9
KellyChimasiah <kellyindeche@gmail.com>
2025-01-31 21:36:59 +0300

    Updated Student.urls logout_view so that when loging out redirects to login page

--- Student/urls.py
+++ Student/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.login_view, name='login'),  # Login view
     path('signup/', views.signup, name='signup'),  # Signup view
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Built-in logout view
+    path('logout/', views.logout_view, name='logout'),  # Built-in logout view
     path('student/dashboard/', views.student_dashboard, name='student_dashboard'),  # Student dashboard view
     path('lecturer/dashboard/', views.lecturer_dashboard, name='lecturer_dashboard'),  # Lecturer dashboard view
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
